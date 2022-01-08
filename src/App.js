0d093af203e11a2aed7a9a5d4dd5d46735a70b12
Patricia Pacheco <patricia.soledad0395@gmail.com>
2022-01-08 17:34:35 -0300

    tp terminado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { Component } from 'react'
+import Gif from './components/Gif';
+import NavBar from './components/NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      gifs : []
+    }
+    this.addGif = this.addGif.bind(this)
+
+  }
+  
+  componentDidMount(){
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=6jo1aqZ0lrquWym83jeWWFUvGIKawqjx&limit=4&rating=g')
+    .then(response => response.json())
+    .then(info => this.setState({
+      gifs : info.data
+    }))
+    .catch(error => console.log(error))
+  }
+
+  componentDidUpdate(){
+    console.log('El componente se a actualizado');
+  }
+
+  addGif(){
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=6jo1aqZ0lrquWym83jeWWFUvGIKawqjx&tag=&rating=g')
+    .then(response => response.json())
+    .then(info => {
+      this.setState({
+      gifs : [
+        ...this.state.gifs,
+        info.data
+      ]
+    })
+  })
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <>
+      <NavBar addGif = {this.addGif} />
+      <div className="container mt-4">
+  
+      <div className="row text-center">
+
+        {
+           this.state.gifs.length === 0
+           ?
+           <p>cargando...</p>
+           :
+          this.state.gifs.map(gif => <Gif key={gif.id} image ={gif.images.original.url} title = {gif.title} />)
+        }
+  
+      </div>
+    </div>
+  
+    </>
+    )
+  }
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const NavBar = ({addGif}) => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed top">
+        <div className="container">
+          <a className="navbar-brand" href="/">GIPHY APP</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="nav ml-auto">
+            <li className="nav-item">
+              <button 
+               className="btn btn-success" 
+               onClick={() => addGif()}>
+                 Cargar random
+                 </button>
+            </li>
+          </ul>
+        </div>
+        </nav>
+    )
+}
+
+export default NavBar
